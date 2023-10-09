6bb9f2afefd6e57a1e0434f645d02538ec7392d3
s4ndc47.github.io <111609431+s4ndc47@users.noreply.github.com>
2023-10-08 23:44:31 -0400

    1144

--- Still_life/fading.js
+++ Still_life/fading.js
@@ -3,11 +3,11 @@ $(document).ready(function() {
     console.log(tenEms);
     var prev_scroll = $(window).scrollTop();
 
-    $('.p1-1text').hide();     // p1
-    $('.p1-2text').hide();  
-    $('#img1-1').hide();     
-    $('#img1-2').hide();    
-  
+    // $('.p1-1').hide();  
+    // $('.p1-2').hide();  
+    // $('.p1-3').hide();  
+    // $('.p1-4').hide();  
+
     var title_top = $('.title').offset().top;
 
     $(window).scroll(function() {
@@ -17,33 +17,44 @@ $(document).ready(function() {
         // scrolling down
         if (prev_scroll < $(window).scrollTop()) {
            
-            if ((title_top - window_top) < 300) {
+            if ((title_top - window_top) < 120) {
             $('.title').stop().fadeTo(500, 0);
             } 
-
-            if (prev_scroll > 4.5 * tenEms) {
-                $(".p1-1text").fadeIn();
+   
+            if (prev_scroll > 20*tenEms) {
+            $('.p1-1').css({opacity: '1'});
             }
-            if (prev_scroll > 4.5 * tenEms) {
-                $("#img1-1").fadeIn();
+
+            if (prev_scroll > 30*tenEms) {
+            $('.p1-2').css({opacity: '1'});
+            }                             
+
+            if (prev_scroll > 40*tenEms) {
+            $('.p1-3').css({opacity: '1'});
             }
-            
 
-         
-         // scrolling up
-         } else {
+            if (prev_scroll > 60*tenEms) {
+            $('.p1-4').css({opacity: '1'});
+            }
 
-             if (prev_scroll < 300) {                            
-                 $('.title').stop().fadeTo('fast', 1)
-              }
+        // scrolling up
+        } else {
 
-             if (prev_scroll < 4.5*tenEms) {                             
-               $(".p1-1text").fadeOut();
-              }
-
-             if (prev_scroll < 4.5*tenEms) {                              
-               $("#img1-1").fadeOut();
-              }
+            if (prev_scroll < 20*tenEms) {
+            $('.p1-1').css({opacity: '0'});
+            }
+    
+             if (prev_scroll < 30*tenEms) {
+            $('.p1-2').css({opacity: '0'});
+            }                             
+    
+            if (prev_scroll < 40*tenEms) {
+            $('.p1-3').css({opacity: '0'});
+            }
+    
+            if (prev_scroll < 60*tenEms) {
+            $('.p1-4').css({opacity: '0'});
+            }
 
         }
     prev_scroll = window_top;
